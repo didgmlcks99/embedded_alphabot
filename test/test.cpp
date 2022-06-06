3ef50cebe8ab5ba83c77bf174472c1f0106a33fb
양희찬 <heechy06@naver.com>
2022-06-06 12:01:30 +0900

    sensor test 1

---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    if(1) cout << "1 is true" << endl;
+    if(0) cout << "0 is true" << endl;
+    if(-1) cout << "-1 is true" << endl;
+
+    return 0x0;
+}
