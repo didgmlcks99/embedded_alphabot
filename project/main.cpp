5459890ef055c3d0657c6eb2f4cd40b5b61f855c
Gahyeon Shim <gagalikewave@gmail.com>
2022-06-08 21:53:49 +0900

    update main.cpp

--- project/main.cpp
+++ project/main.cpp
@@ -1,6 +1,6 @@
 #include "mbed.h"
 #include "TRSensors.h"
-//#include "PID.h"
+#include "PID.h"
 
 UnbufferedSerial pc(CONSOLE_TX, CONSOLE_RX, 230400);
 
@@ -11,15 +11,15 @@ TRSensors TR(D11, D12, D13, D10);
 #define RATE 0.1
  
 //Kc, Ti, Td, interval
-//PID controller(1.0, 0.0, 0.0, RATE);
+PID controller(1.0, 0.0, 0.0, RATE);
 /**********Motor********************/
-PwmOut PWMA(D6);  //PB_10,D6
-PwmOut PWMB(D5);
+PwmOut PWML(D6);  //PB_10,D6
+PwmOut PWMR(D5);
 
-DigitalOut AIN1(A1);
-DigitalOut AIN2(A0);
-DigitalOut BIN1(A2);
-DigitalOut BIN2(A3);
+DigitalOut LIN1(A0); // left motor backward
+DigitalOut LIN2(A1); // left motor forward
+DigitalOut RIN3(A2); // right motor backward
+DigitalOut RIN4(A3); // right motor forward
 
 #define MIDDLE 2000
 int R_PWM;  
@@ -43,46 +43,45 @@ int main() {
     int result = 0;
     Motor_init();
     
+    PWMR.pulsewidth_ms(100);
+    PWML.pulsewidth_ms(100);
+    PWMR = 0.2; PWML = 0.2;
+    
     unsigned int sensor_values[5] = {0};
     
     ThisThread::sleep_for(3000ms);
-    
-    printf("Start alphabot!\r\n");
-    
     while(1) {
         TR.calibrate();
         result = TR.readLine(sensor_values, 1);
         printf("result = %d, sensor values: %u %u %u %u %u\r\n", 
             result, sensor_values[0], sensor_values[1], sensor_values[2], sensor_values[3], sensor_values[4]);
-        ThisThread::sleep_for(100ms);
+        ThisThread::sleep_for(1000ms);
     }
 }
 
 
 /********************Motor********************/
-void Motor_Stop(){
-    printf("===============================Motor Stop!\r\n");
-    AIN1 = 0;
-    AIN2 = 0;
-    BIN1 = 0;
-    BIN2 = 0;
-    PWMB.pulsewidth_us(0);
-    PWMA.pulsewidth_us(0);
-}
-
 void Motor_init(){
-    AIN1 = 0;
-    AIN2 = 1;
-    BIN1 = 0;
-    BIN2 = 1;
-    PWMB.period_us(500);
-    PWMA.period_us(500);
+    LIN1 = 0;
+    LIN2 = 1;
+    RIN3 = 1;
+    RIN4 = 0;
+    PWML.period_ms(100);
+    PWMR.period_ms(100);
     
     den = 0;
     R_PWM = 0;
     L_PWM = 0;
     weight= 0;
     print_c = 0;
+}
 
-    
+void Motor_Stop(){
+    //printf("===============================Motor Stop!\r\n");
+    LIN1 = 0;
+    LIN2 = 0;
+    RIN3 = 0;
+    RIN4 = 0;
+    PWML.pulsewidth_ms(0);
+    PWMR.pulsewidth_ms(0);
 }
